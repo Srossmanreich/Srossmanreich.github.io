891c894a3f00be68f3e4c16ee9dade669d17a9a7
Sydney Rossman-Reich <srossmanreich@gmail.com>
2015-12-22 08:54:58 -0800

    Add technical blog rb code to github

---
+++ blogs/test.rb
@@ -0,0 +1,19 @@
+#Making empty arrays and hashes
+
+array1 = Array.new
+hash1 = Hash.new
+
+array2 = []
+hash2 = {}
+
+#Making nonempty arrays and hashes
+
+array1 = [1,"two",3.0]
+hash1 = { "Sydney" => "Icecream", "Ryan" => "Cake"}
+hash1_symbols = { :Sydney => "Icecream", :Ryan => "Cake"}
+hash1_symbols_again = { Sydney: "Icecream", Ryan: "Cake"}
+
+array2 = Array.new(3,"this object will appear in each of 3 spots in the array")
+
+array3[0] = "object at index 0"
+hash3["Sydney"] = "Icecream"
